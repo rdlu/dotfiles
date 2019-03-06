a9cbe7154ac841472e1674753ef24fe602af88ad
Rodrigo Dlugokenski <r@dlu.one>
2019-03-06 13:53:51 -0300

    Virtual Env Disable Prompt for Py

--- fish/variables.fish
+++ fish/variables.fish
@@ -13,5 +13,6 @@ set -gx BD_OPT 'insensitive'
 mkdir -p $HOME/.local/bin $NPM_PACKAGES/bin $PATH $HOME/.fzf
 set -gx PATH $HOME/.local/bin $NPM_PACKAGES/bin $PATH $HOME/.fzf
 
-# Python PIP
+# Python PIP and VENV
 set -gx PYTHONPATH (python -c "import site, os; print(os.path.join(site.USER_BASE, 'lib', 'python', 'site-packages'))"):$PYTHONPATH
+set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
