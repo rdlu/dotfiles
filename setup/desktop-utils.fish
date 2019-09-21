38d7c06d9414c7505e1d630f81c6cb57dd1d53cb
Rodrigo Dlugokenski <r@dlu.one>
2019-09-21 16:12:19 -0300

    fonts

--- setup/desktop-utils.fish
+++ setup/desktop-utils.fish
@@ -1,7 +1,10 @@
 #! /usr/bin/fish
 cd
-yay -S --noconfirm albert-lite visual-studio-code-bin ttf-iosevka   \
-    pinta simple-scan xsel xclip                        \
-    adobe-source-sans-pro-fonts chromium clementine     \
-    deluge firefox-developer-edition mosh otf-fira-code \
-    otf-fira-sans p7zip ttf-roboto ttf-ubuntu-font-family
+yay -S --noconfirm albert-lite visual-studio-code-bin   \
+    pinta simple-scan xsel xclip chromium clementine    \
+    deluge firefox-developer-edition mosh p7zip
+
+# Nice fonts
+yay -S ttf-windows ttf-iosevka otf-fira-code        \
+        adobe-source-sans-pro-fonts otf-fira-sans   \
+        ttf-roboto ttf-ubuntu-font-family ttf-cascadia-code
