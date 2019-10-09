477c7f300396929e294028ee8c760012dacbe64a
Rodrigo Dlugokenski <r@dlu.one>
2019-10-08 21:02:55 -0300

    moar packages

--- setup/desktop-utils.fish
+++ setup/desktop-utils.fish
@@ -5,6 +5,6 @@ yay -S --noconfirm albert-lite visual-studio-code-bin   \
     deluge firefox-developer-edition mosh p7zip
 
 # Nice fonts
-yay -S ttf-windows ttf-iosevka otf-fira-code        \
+yay -S --needed ttf-windows ttf-iosevka otf-fira-code        \
         adobe-source-sans-pro-fonts otf-fira-sans   \
         ttf-roboto ttf-ubuntu-font-family ttf-cascadia-code
