c6e90fd55569813bab88cd9fb8a452b3aa993670
Rodrigo Dlugokenski <r@dlu.one>
2019-11-03 00:06:52 -0300

    virtualfish added to fish shell

--- fish/variables.fish
+++ fish/variables.fish
@@ -16,3 +16,6 @@ set -gx PATH $HOME/.local/bin $HOME/.yarn/bin $NPM_PACKAGES/bin $PATH $HOME/.fzf
 # Python PIP and VENV
 set -gx PYTHONPATH (python -c "import site, os; print(os.path.join(site.USER_BASE, 'lib', 'python', 'site-packages'))"):$PYTHONPATH
 set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
+
+
+set -gx PROJECT_HOME $HOME/Projects
