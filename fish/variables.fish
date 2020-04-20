e3b436903c4c9d510fd41086b39985f12b7abc56
Rodrigo Dlugokenski <rddweb@gmail.com>
2020-04-20 14:20:11 -0300

    adding cargo bin path and nodenv

--- fish/variables.fish
+++ fish/variables.fish
@@ -11,7 +11,7 @@ set -gx GOPATH ~/Apps/go
 set -gx BD_OPT 'insensitive'
 
 mkdir -p $HOME/.local/bin $NPM_PACKAGES/bin $PATH $HOME/.fzf
-set -gx PATH $HOME/.local/bin $HOME/.yarn/bin $NPM_PACKAGES/bin $PATH $HOME/.fzf
+set -gx PATH $HOME/.local/bin $HOME/.yarn/bin $HOME/.cargo/bin $NPM_PACKAGES/bin $PATH $HOME/.fzf
 
 # Python PIP and VENV
 set -gx PYTHONPATH (python -c "import site, os; print(os.path.join(site.USER_BASE, 'lib', 'python', 'site-packages'))"):$PYTHONPATH
