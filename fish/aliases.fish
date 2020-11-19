5a26a4160cbdcc13bd218bddb067e69501cb4604
Rodrigo Dlugokenski <rddweb@gmail.com>
2020-11-19 08:34:06 -0300

    More docker-compose abbrs

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,4 +1,4 @@
-# abbreviations
+# Git related
 abbr -a gf git fetch --all
 abbr -a gps git push
 abbr -a gpsa 'git pushall'
@@ -8,22 +8,33 @@ abbr -a gcm git commit -am
 abbr -a gsw 'git switch'
 abbr -a cpv 'sudo rsync --info=progress2 -avhW --no-compress'
 abbr -a gst git status
+abbr -a git-rename-main 'git branch -m master main; git push -u origin main'
+
+# Filesystem
 abbr -a tf tail -f
 abbr -a lsh ls -lah
 abbr -a lsd 'ls -d */'
-abbr -a git-rename-main 'git branch -m master main; git push -u origin main'
+abbr -a mkdir mkdir -pv
+alias ls 'ls --color'
+alias path 'echo -e {$PATH\n}'
+alias less 'less -FSRXc'
 
+# Terminal management
+abbr -a mux tmux new -A -s mux0 fish
+abbr -a mux1 tmux new -A -s mux1 fish
+
+# Node.js
+abbr -a npmkill find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' +
+abbr -a rm-npm-modules find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' +
+
+# System management
 abbr -a pacman sudo pacman
 abbr -a systart sudo systemctl start
 abbr -a systatus sudo systemctl status
 abbr -a sysnable sudo systemctl enable --now
 abbr -a systop sudo systemctl stop
-abbr -a mux tmux new -A -s mux0 fish
-abbr -a mux1 tmux new -A -s mux1 fish
-abbr -a mkdir mkdir -pv
-abbr -a npmkill find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' +
-abbr -a rm-npm-modules find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' +
 
+# Networking
 abbr -a sockets-list 'sudo /usr/sbin/lsof -i -P'             # lsock:        Display open sockets
 abbr -a sockets-list-udp 'sudo /usr/sbin/lsof -nP | grep UDP'   # lsockU:       Display only open UDP sockets
 abbr -a sockets-list-tcp 'sudo /usr/sbin/lsof -nP | grep TCP'   # lsockT:       Display only open TCP sockets
@@ -33,15 +44,18 @@ abbr -a myip2 'dig TXT +short o-o.myaddr.l.google.com @ns1.google.com'
 abbr -a gateway-ip-address 'ip route list | awk \' /^default/ {print $3}\''
 abbr -a ping-gateway 'ping (ip route list | awk \' /^default/ {print $3}\')'
 
-alias path 'echo -e {$PATH\n}'
-alias less 'less -FSRXc'
+# Graphics
 alias nvrun 'set -lx __NV_PRIME_RENDER_OFFLOAD 1; set -lx __GLX_VENDOR_LIBRARY_NAME nvidia;'
-alias ls 'ls --color'
 
 # Docker Management
 abbr -a dkc docker-compose
 abbr -a dkc-logs 'docker-compose logs -f'
 abbr -a dkcx 'docker-compose exec'
+abbr -a dkcs 'docker-compose start'
+abbr -a dkc-start 'docker-compose start'
+abbr -a dkc-stop 'docker-compose stop'
+abbr -a dkcup 'docker-compose up -d'
+abbr -a dkcdn 'docker-compose down'
 function dkcx --description 'Executes $PROGRAM in $MACHINE'
   docker-compose exec $argv[2] $argv[1]
 end
