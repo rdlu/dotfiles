475661f87a86e93d8d95f0edf962ad44f0193f96
Rodrigo Dlugokenski <rddweb@gmail.com>
2021-03-09 10:43:32 -0300

    git reset last commit

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -14,6 +14,7 @@ abbr -a gck-b 'git checkout -b'
 abbr -a gck-clean 'git-checkout-clean'
 abbr -a gck-m 'git-checkout-pull master'
 abbr -a git-set-upstream 'git push --set-upstream origin (git branch --show-current)'
+abbr -a git-undo-last 'git reset --soft HEAD~1'
 
 
 # Filesystem
