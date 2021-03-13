80f804124b389e15b865feddddf8613fa4dfe530
Rodrigo Dlugokenski <109541+rdlu@users.noreply.github.com>
2021-03-13 18:33:20 -0300

    Checking local rbenv for fedora

--- fish/conf.d/rbenv.fish
+++ fish/conf.d/rbenv.fish
@@ -1,3 +1,9 @@
+set RBENV_DIR $HOME/.rbenv
+if test -e $RBENV_DIR/bin
+  set -gx PATH $PATH $RBENV/bin
+end
+
+
 which rbenv > /dev/null
 if test $status -eq 0
     status --is-interactive; and source (rbenv init -|psub)
