437988786f81d97f4ac872885eef6afc70da0f45
Rodrigo Dlugokenski <rdlugokenski@pwnhealth.com>
2021-04-27 14:45:40 -0300

    Moving the asdf test to the right place

--- fish/conf.d/asdf.fish
+++ fish/conf.d/asdf.fish
@@ -5,3 +5,6 @@ if [ -e  /opt/asdf-vm/asdf.fish ] ; source /opt/asdf-vm/asdf.fish ; end
 
 # MacOS / Homebrew
 if [ -e  /usr/local/opt/asdf/asdf.fish ] ; source /usr/local/opt/asdf/asdf.fish ; end
+
+# Git version
+test -e {$HOME}/.asdf/asdf.fish ; and source {$HOME}/.asdf/asdf.fish
--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,6 @@ for snippet in {$HOME}/.dotfiles/fish/conf.d/*
     source $snippet
 end
 
-test -e {$HOME}/.asdf/asdf.fish ; and source {$HOME}/.asdf/asdf.fish
-
 test -e {$HOME}/.dotfiles/fish/variables.fish ; and source {$HOME}/.dotfiles/fish/variables.fish
 test -e {$HOME}/.dotfiles/fish/aliases.fish ; and source {$HOME}/.dotfiles/fish/aliases.fish
 test -e {$HOME}/.dotfiles/fish/functions.fish ; and source {$HOME}/.dotfiles/fish/functions.fish
