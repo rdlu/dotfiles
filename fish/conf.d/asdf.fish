99c3191c349fdf55945fa3fd77832fe5a629c9ec
Rodrigo Dlugokenski <rdlugokenski@pwnhealth.com>
2021-04-27 14:23:39 -0300

    asdf using homebrew on mac

--- fish/conf.d/asdf.fish
+++ fish/conf.d/asdf.fish
@@ -2,3 +2,6 @@
 
 # Test Pacman / AUR version
 if [ -e  /opt/asdf-vm/asdf.fish ] ; source /opt/asdf-vm/asdf.fish ; end
+
+# MacOS / Homebrew
+if [ -e  /usr/local/opt/asdf/asdf.fish ] ; source /usr/local/opt/asdf/asdf.fish ; end
