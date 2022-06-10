2375164ecaf0c20a5a277da4233e11d7b16726ed
Rodrigo Dlugokenski <rodrigo.git@dlu.one>
2022-06-10 20:22:10 -0300

    adding rsync to packs

--- setup/archlinux-extras.fish
+++ setup/archlinux-extras.fish
@@ -4,7 +4,7 @@
 sudo sed -i 's/#Color/Color/g' /etc/pacman.conf
 
 sudo pacman -Suy
-sudo pacman -Sy --needed tmux fpp fzf thefuck git shfmt lsb-release bat exa
+sudo pacman -Sy --needed tmux fpp fzf thefuck git shfmt lsb-release bat exa rsync
 
 switch (lsb_release -is)
 case ManjaroLinux
@@ -22,7 +22,7 @@ case Arch
     end
 end
 
-yay -S --needed tldr rbenv ruby-build elixir autoconf automake  \
+yay -S --needed tldr ruby-build elixir autoconf automake  \
     bison bind-tools fasd htop make patch ed fzf gcc mosh ruby ruby-rdoc tk yarn \
     php php-fpm python-pip \
     nfs-utils lsof strace python-pyusb mosh
