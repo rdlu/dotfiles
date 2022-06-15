e61d2d35269316d571ce76feb589ba25f61a3244
Rodrigo Dlugokenski <rodrigo.git@dlu.one>
2022-06-15 10:04:01 -0300

    asdf conf checks completion

--- fish/conf.d/asdf.fish
+++ fish/conf.d/asdf.fish
@@ -1,10 +1,22 @@
 # Initialize ASDF
 
+set COMPLETION_SCRIPT ~/.asdf/completions/asdf.fish
+
 # Test Pacman / AUR version
-if [ -e  /opt/asdf-vm/asdf.fish ] ; source /opt/asdf-vm/asdf.fish ; end
+if [ -e  /opt/asdf-vm/asdf.fish ]
+    source /opt/asdf-vm/asdf.fish
+    set COMPLETION_SCRIPT /opt/asdf-vm/completions/asdf.fish
+end
 
 # MacOS / Homebrew
-if [ -e  /usr/local/opt/asdf/asdf.fish ] ; source /usr/local/opt/asdf/asdf.fish ; end
+if [ -e  /usr/local/opt/asdf/asdf.fish ]
+    source /usr/local/opt/asdf/asdf.fish
+    set COMPLETION_SCRIPT /usr/local/opt/asdf/asdf.fish
+end
 
 # Git version
 test -e {$HOME}/.asdf/asdf.fish ; and source {$HOME}/.asdf/asdf.fish
+
+if not test -e ~/.config/fish/completions/asdf.fish
+    mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+end
