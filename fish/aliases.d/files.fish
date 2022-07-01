fdf0e15240bcd7d12e1ea1cca302b7b19aa5bd88
Rodrigo Dlugokenski <git@dlu.one>
2022-07-01 11:01:10 -0300

    exa in interactive mode

--- fish/aliases.d/files.fish
+++ fish/aliases.d/files.fish
@@ -6,26 +6,26 @@ abbr -a lsd 'ls -d */'
 abbr -a mkdir mkdir -pv
 alias path 'echo -e {$PATH\n}'
 alias less 'less -FSRXc'
-alias mv "mv -i"  # "m" - never forget
+alias mv "mv -i" # "m" - never forget
 alias cp "cp -i"
 alias b "cd - >/dev/null && l" # b stands for back
 
 if test (uname) != Darwin
-  alias ls 'ls --color'
+    alias ls 'ls --color'
 end
 
-if type -q exa
-  alias l "exa --time-style long-iso --color=auto -F"
-  alias ll "l -Fahl"
-  alias la "l -a"
+if type -q exa; and status is-interactive
+    alias l "exa --time-style long-iso --color=auto -F"
+    alias ll "l -Fahl"
+    alias la "l -a"
 else
-  alias l "ls --color=auto -F"
-  alias ll "l -Ahl"
-  alias la "l -a"
+    alias l "ls --color=auto -F"
+    alias ll "l -Ahl"
+    alias la "l -a"
 end
 
 if type -q bat; and status is-interactive
-  alias cat bat
+    alias cat bat
 end
 
 # cd aliases
