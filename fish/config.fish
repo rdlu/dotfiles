fd89195e1f96e37f7fa8f655a7e54e51a2daecfd
Rodrigo Dlugokenski <git@dlu.one>
2022-07-04 07:58:22 -0300

    ignoring auto tmux inside vscode terminal

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,10 @@ for snippet in {$HOME}/.dotfiles/fish/conf.d/*
     source $snippet
 end
 
-test -e {$HOME}/.dotfiles/fish/variables.fish ; and source {$HOME}/.dotfiles/fish/variables.fish
-test -e {$HOME}/.dotfiles/fish/aliases.fish ; and source {$HOME}/.dotfiles/fish/aliases.fish
-test -e {$HOME}/.dotfiles/fish/functions.fish ; and source {$HOME}/.dotfiles/fish/functions.fish
-test -e {$HOME}/.config/fish/config.local ; and source {$HOME}/.config/fish/config.local
+test -e {$HOME}/.dotfiles/fish/variables.fish; and source {$HOME}/.dotfiles/fish/variables.fish
+test -e {$HOME}/.dotfiles/fish/aliases.fish; and source {$HOME}/.dotfiles/fish/aliases.fish
+test -e {$HOME}/.dotfiles/fish/functions.fish; and source {$HOME}/.dotfiles/fish/functions.fish
+test -e {$HOME}/.config/fish/config.local; and source {$HOME}/.config/fish/config.local
 
 # test $TERM != "screen-256color"; and exec tmux new -A -s mux0 fish
 
@@ -18,8 +18,10 @@ if not type -q fisher
 end
 
 # starhip.rs
-if type -q starship; starship init fish | source; end 
+if type -q starship
+    starship init fish | source
+end
 
-if set -q START_TMUX; and not set -q TMUX; and status is-interactive
+if set -q START_TMUX; and not set -q TMUX; and status is-interactive; and not set -q INSIDE_EDITOR
     tmux new -A -s mux0 fish
 end
