eb8114fa1c44db06bb89526594048c495cb3844c
Rodrigo Dlugokenski <git@dlu.one>
2023-01-28 08:45:24 -0300

    gnome changes

---
+++ setup/gnome-dconf.fish
@@ -0,0 +1,12 @@
+#! .usr.bin.fish
+
+echo 'Set Fractional Scaling in Wayland'
+
+gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
+
+echo 'Remove Alt+` as shortcut (used by some terminal apps)'
+gsettings get org.gnome.desktop.wm.keybindings switch-group
+gsettings set org.gnome.desktop.wm.keybindings switch-group "['<Super>Above_Tab']"
+
+echo 'Set Custom Print Screen (MX Mechanical Mini)'
+gsettings set org.gnome.shell.keybindings show-screenshot-ui "['Print', '<Shift><Super>s']"
