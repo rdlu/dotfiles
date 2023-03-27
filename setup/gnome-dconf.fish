a44cbd0f192fcac28b1e9d2e676cee8094f192b0
Rodrigo Dlugokenski <git@dlu.one>
2023-03-27 08:11:57 -0300

    Fixing some gnome shortcuts overriden by pop shell

--- setup/gnome-dconf.fish
+++ setup/gnome-dconf.fish
@@ -1,4 +1,4 @@
-#! .usr.bin.fish
+#! /usr/bin/fish
 
 echo 'Set Fractional Scaling in Wayland'
 
@@ -10,3 +10,10 @@ gsettings set org.gnome.desktop.wm.keybindings switch-group "['<Super>Above_Tab'
 
 echo 'Set Custom Print Screen (MX Mechanical Mini)'
 gsettings set org.gnome.shell.keybindings show-screenshot-ui "['Print', '<Shift><Super>s']"
+
+
+echo 'Restore Pop Shell Shortcuts stolen'
+gsettings set org.gnome.shell.extensions.pop-shell focus-down "['<Control><Super>Down']"
+gsettings set org.gnome.shell.extensions.pop-shell focus-left "['<Control><Super>Left']"
+gsettings set org.gnome.shell.extensions.pop-shell focus-right "['<Control><Super>Right']"
+gsettings set org.gnome.shell.extensions.pop-shell focus-up "['<Control><Super>Up']"
