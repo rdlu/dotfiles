7a9003f7f7e73de7af28c59d2aef5725d923aadb
Rodrigo Dlugokenski <git@dlu.one>
2023-04-07 10:02:44 -0300

    Adding btm a system tui monitor

--- setup/fedora-base.fish
+++ setup/fedora-base.fish
@@ -1,7 +1,11 @@
 #! /usr/bin/bash
 sudo dnf copr enable atim/lazygit -y
-sudo dnf install -y ripgrep lazygit neovim fd-find yarn python3-neovim python3-pip gcc-c++
+sudo dnf install -y ripgrep lazygit neovim fd-find yarn python3-neovim python3-pip gcc-c++ git-delta
 sudo yum groupinstall 'Development Tools'
 
 sudo dnf install -y rubygem-rails
 sudo dnf install -y yubikey-manager-qt
+
+
+sudo dnf copr enable atim/bottom -y
+sudo dnf install bottom
