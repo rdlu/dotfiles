cbbf8ab80b4588b8dfc6f4af91a186ab11fc90da
Rodrigo Dlugokenski <git@dlu.one>
2023-04-26 14:09:05 -0300

    nvim switcher, git default branch

---
+++ fish/conf.d/nvim-switcher.fish
@@ -0,0 +1,18 @@
+function nvim-lazy
+    env NVIM_APPNAME=nvim-lazy nvim
+end
+
+function nvims
+    set items default nvim-lazy
+    set config (printf "%s\n" $items | fzf --prompt=" Neovim Config = " --height=~50% --layout=reverse --border --exit-0)
+    if [ -z $config ]
+        echo "Nothing selected"
+        return 0
+    else if [ $config = default ]
+        set config ""
+    end
+
+    env NVIM_APPNAME=$config nvim $argv
+end
+
+bind \ca nvims
