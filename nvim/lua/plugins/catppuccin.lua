8e7747b91c7a65cd40880b7329f4ae4e0f049028
Rodrigo Dlugokenski <git@dlu.one>
2023-05-22 16:51:23 -0300

    neovim

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+      --     dim_inactive = {
+      --       enabled = true,
+      --       shade = "dark",
+      --       percentage = 0.9,
+      --     },
+    },
+  },
+}
