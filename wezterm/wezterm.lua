a85cfee3d16f287bc31cc056622b32f405a6eeac
Rodrigo Dlugokenski <git@dlu.one>
2023-05-22 16:26:40 -0300

    wezterm: some configs for another shot

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,7 +7,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -15,18 +15,35 @@ end
 -- For example, changing the color scheme:
 -- config.color_scheme = 'AdventureTime'
 -- config.color_scheme = 'Dracula+'
-config.color_scheme = 'Tango (terminal.sexy)'
+config.color_scheme = "Tango (terminal.sexy)"
 config.colors = {
-  -- Make the selection text color fully transparent.
-  -- When fully transparent, the current text color will be used.
-  selection_fg = 'none',
-  -- Set the selection background color with alpha.
-  -- When selection_bg is transparent, it will be alpha blended over
-  -- the current cell background color, rather than replace it
-  selection_bg = 'rgba(50% 50% 50% 50%)',
+	-- Make the selection text color fully transparent.
+	-- When fully transparent, the current text color will be used.
+	selection_fg = "none",
+	-- Set the selection background color with alpha.
+	-- When selection_bg is transparent, it will be alpha blended over
+	-- the current cell background color, rather than replace it
+	selection_bg = "rgba(50% 50% 50% 50%)",
 }
+
+config.font_size = 11
+config.font = wezterm.font_with_fallback {
+  'Mononoki Nerd Font',
+  'FantasqueSansMono Nerd Font',
+}
+
+
 config.window_background_opacity = 0.8
 config.hide_tab_bar_if_only_one_tab = true
+
+config.default_cursor_style = "BlinkingBlock"
+config.window_decorations = "RESIZE"
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 
 
 -- and finally, return the configuration to wezterm
