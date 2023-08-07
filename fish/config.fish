e894745a2ae9b159cddb0622d4b79b136895652e
Rodrigo Dlugokenski <git@dlu.one>
2023-08-07 08:41:50 -0300

    envman updt to fish

--- fish/config.fish
+++ fish/config.fish
@@ -25,3 +25,6 @@ end
 if set -q START_TMUX; and not set -q TMUX; and status is-interactive; and not set -q INSIDE_EDITOR
     tmux new -A -s mux0 fish
 end
+
+# Generated for envman. Do not edit.
+test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
