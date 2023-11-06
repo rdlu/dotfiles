e7b38221226571a51377966e487c6c9f93464d03
Rodrigo Dlugokenski <git@dlu.one>
2023-11-06 08:02:34 -0300

    ruby install instructions for old gems

--- fish/conf.d/rbenv.fish
+++ fish/conf.d/rbenv.fish
@@ -1,9 +1,13 @@
 set RBENV_DIR $HOME/.rbenv
 if test -e $RBENV_DIR/bin
-  set -gx PATH $PATH $RBENV/bin
+    set -gx PATH $PATH $RBENV/bin
 end
 
 
-if type -q rbenv; and status --is-interactive;
-   source (rbenv init -|psub)
+if type -q rbenv; and status --is-interactive
+    source (rbenv init -|psub)
 end
+
+# Custom openssl1.1 for older Ruby libs and tools using ancient ssl
+# sudo dnf install openssl1.1-devel
+# set -lx RUBY_CONFIGURE_OPTS '--with-openssl-dir=/usr/include/openssl11 --with-openssl-lib=/usr/lib64/openssl11 --with-openssl-include=/usr/include/openssl11'
