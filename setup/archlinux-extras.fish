2724c73db5f0cb360919b657470a8e6986415260
Rodrigo Dlugokenski <git@dlu.one>
2023-11-27 07:27:30 -0300

    updating arch setup

--- setup/archlinux-extras.fish
+++ setup/archlinux-extras.fish
@@ -4,25 +4,23 @@
 sudo sed -i 's/#Color/Color/g' /etc/pacman.conf
 
 sudo pacman -Suy
-sudo pacman -Sy --needed tmux fpp fzf thefuck git shfmt lsb-release bat exa rsync
+sudo pacman -Sy --needed base-devel tmux fpp thefuck git shfmt lsb-release bat exa rsync
 
 switch (lsb_release -is)
-case ManjaroLinux
-    sudo pacman -Sy --needed yay
-case EndeavourOS
-    sudo pacman -Sy --needed yay
-case Arch
-    if type -q yay
-        echo "Yay already installed"
-    else
-        cd /tmp/
-        git clone https://aur.archlinux.org/yay.git
-        cd yay
-        makepkg -si
-    end
+    case ManjaroLinux
+        sudo pacman -Sy --needed yay
+    case EndeavourOS
+        sudo pacman -Sy --needed yay
+    case Arch
+        if type -q yay
+            echo "Yay already installed"
+        else
+            cd /tmp/
+            sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+        end
 end
 
-yay -S --needed tldr ruby-build elixir autoconf automake  \
+yay -S --needed tldr ruby-build elixir autoconf automake \
     bison bind-tools fasd htop make patch ed fzf gcc mosh ruby ruby-rdoc tk yarn \
     php php-fpm python-pip \
     nfs-utils lsof strace python-pyusb mosh
