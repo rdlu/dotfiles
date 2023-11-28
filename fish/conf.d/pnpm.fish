37f7c95d885ab1e374171a016cfc5b0c1b7326c4
Rodrigo Dlugokenski <git@dlu.one>
2023-11-28 09:07:14 -0300

    npm dirrs

--- fish/conf.d/pnpm.fish
+++ fish/conf.d/pnpm.fish
@@ -1,5 +1,5 @@
 # pnpm
-set -gx PNPM_HOME "/home/rdlu/.local/share/pnpm"
+set -gx PNPM_HOME $HOME/.local/share/pnpm
 if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
