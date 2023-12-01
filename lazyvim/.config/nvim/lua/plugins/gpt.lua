73c0ef22be8525f11d7b535ec4b791ef0e0902f9
Rodrigo Dlugokenski <git@dlu.one>
2023-12-01 09:06:29 -0300

    neovim gpt: using the new turbo model

--- lazyvim/.config/nvim/lua/plugins/gpt.lua
+++ lazyvim/.config/nvim/lua/plugins/gpt.lua
@@ -1,12 +1,16 @@
 return {
-  "jackMort/ChatGPT.nvim",
-  event = "VeryLazy",
-  config = function()
-    require("chatgpt").setup()
-  end,
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
-  },
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+        local cfg = {
+            openai_params = {
+                model = "gpt-3.5-turbo-1106"
+            },
+            openai_edit_params = {
+                model = "gpt-3.5-turbo-1106"
+            }
+        }
+        require("chatgpt").setup(cfg)
+    end,
+    dependencies = {"MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim"}
 }
