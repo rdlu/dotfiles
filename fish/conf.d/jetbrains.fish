63a455bcb3709abbc48e29749f04b692081f49ee
Rodrigo Dlugokenski <git@dlu.one>
2024-01-02 08:53:37 -0300

    fish python: paths

---
+++ fish/conf.d/jetbrains.fish
@@ -0,0 +1,3 @@
+if test -e $HOME/.local/share/JetBrains/Toolbox/scripts
+    set -gx PATH $HOME/.local/share/JetBrains/Toolbox/scripts $PATH
+end
--- fish/conf.d/python.fish
+++ fish/conf.d/python.fish
@@ -1,7 +1,31 @@
 # Python PIP and VENV
-if type -q python
-    set -gx PYTHONPATH (python -c "import site, os; print(os.path.join(site.USER_BASE, 'lib', 'python', 'site-packages'))"):$PYTHONPATH
-end
-set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
+#if type -q python
+#    set -gx PYTHONPATH (python -c "import site, os; print(os.path.join(site.USER_BASE, 'lib', 'python', 'site-packages'))"):$PYTHONPATH
+#end
+#set -gx VIRTUAL_ENV_DISABLE_PROMPT 0
 # disabling for rtx/asdf
-set -gx PIP_REQUIRE_VIRTUALENV 0
+#set -gx PIP_REQUIRE_VIRTUALENV 0
+
+# thanks https://alexwlchan.net/2023/fish-venv/
+function venv --description "Create and activate a new virtual environment"
+    if test -e .venv
+        set_color purple
+        echo "Activating python venv using fish"
+        source .venv/bin/activate.fish
+    else
+        echo "Creating virtual environment in "(pwd)"/.venv"
+        python3 -m venv .venv --upgrade-deps
+        source .venv/bin/activate.fish
+
+        # Append .venv to the Git exclude file, but only if it's not
+        # already there.    
+        if test -e .git
+            set line_to_append ".venv"
+            set target_file ".git/info/exclude"
+
+            if not grep --quiet --fixed-strings --line-regexp "$line_to_append" "$target_file" 2>/dev/null
+                echo "$line_to_append" >>"$target_file"
+            end
+        end
+    end
+end
