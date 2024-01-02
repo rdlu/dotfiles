a94b988bf66cd0f964698224c4afc73f034b8645
Rodrigo Dlugokenski <git@dlu.one>
2024-01-02 08:54:32 -0300

    lazyvim: neotest for py and obsidian

---
+++ lazyvim/.config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/neotest-python",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      adapters = {
+        ["neotest-python"] = {
+          -- Here you can specify the settings for the adapter, i.e.
+          runner = "pytest",
+          -- python = ".venv/bin/python",
+        },
+      },
+    },
+  },
+}
