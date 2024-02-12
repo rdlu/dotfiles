997c4fa74d3356498a61ab608200e4cd9b100ffc
Rodrigo Dlugokenski <git@dlu.one>
2024-02-11 21:48:05 -0300

    rename rtx to mise for bash

--- terminal/.config/bash.d/evals.bash
+++ terminal/.config/bash.d/evals.bash
@@ -1,23 +1,23 @@
-if command -v starship &> /dev/null; then
-  eval "$(starship init bash)"
+if command -v starship &>/dev/null; then
+	eval "$(starship init bash)"
 else
-  echo "Starship is not installed. You can install it from https://starship.rs/"
+	echo "Starship is not installed. You can install it from https://starship.rs/"
 fi
 
-if command -v rtx &> /dev/null; then
-  eval "$(rtx activate bash)"
+if command -v mise &>/dev/null; then
+	eval "$(mise activate bash)"
 fi
 
 if [ -f "$HOME/.cargo/env" ]; then
-  source "$HOME/.cargo/env"
+	source "$HOME/.cargo/env"
 fi
 
-if command -v direnv &> /dev/null; then
-  eval "$(direnv hook bash)"
+if command -v direnv &>/dev/null; then
+	eval "$(direnv hook bash)"
 fi
 
 [ -s "$HOME/.config/envman/load.sh" ] && source "$HOME/.config/envman/load.sh"
 
 if [[ "$START_TMUX" && ! "$TMUX" && -t 0 && ! "$INSIDE_EDITOR" ]]; then
-    tmux new -A -s mux0 fish
+	tmux new -A -s mux0 fish
 fi
