15f3a45d8ea4c6f23677c24d14d54f3324d5e833
Rodrigo Dlugokenski <git@dlu.one>
2024-03-20 07:12:38 -0300

    harpoon and elixir

--- lazyvim/.config/nvim/lua/plugins/elixir.lua
+++ lazyvim/.config/nvim/lua/plugins/elixir.lua
@@ -1,29 +1,77 @@
 return {
-  "elixir-tools/elixir-tools.nvim",
-  version = "*",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local elixir = require("elixir")
-    local elixirls = require("elixir.elixirls")
+  {
+    "elixir-tools/elixir-tools.nvim",
+    version = "*",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local elixir = require("elixir")
 
-    elixir.setup({
-      nextls = { enable = true },
-      credo = {},
-      elixirls = {
-        enable = true,
-        settings = elixirls.settings({
-          dialyzerEnabled = false,
-          enableTestLenses = true,
-        }),
-        on_attach = function(client, bufnr)
-          vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-          vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-          vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
-        end,
+      elixir.setup({
+        nextls = {
+          enable = true,
+          experimental = {
+            completions = {
+              enable = true,
+            },
+          },
+        },
+        credo = {
+          enable = true,
+        },
+        elixirls = {
+          enable = false,
+        },
+      })
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "elixir",
+        "heex",
+        "eex",
+      })
+    end,
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "jfpedroza/neotest-elixir",
+    },
+    opts = {
+      adapters = {
+        ["neotest-elixir"] = {},
       },
-    })
-  end,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      if vim.fn.executable("credo") == 0 then
+        return
+      end
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.diagnostics.credo,
+      })
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = function(_, opts)
+      if vim.fn.executable("credo") == 0 then
+        return
+      end
+      opts.linters_by_ft = {
+        elixir = { "credo" },
+      }
+    end,
   },
 }
