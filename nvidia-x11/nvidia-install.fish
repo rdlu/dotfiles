a172e1da9f6e25422bcb1caf4f4669977fae228e
Rodrigo Dlugokenski <git@dlu.one>
2024-03-26 13:03:46 -0300

    nvidia remove

--- nvidia-x11/nvidia-install.fish
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/fish
-
-# install
-yay -S --needed --noconfirm nvidia nvidia-utils nvidia-settings cuda
-
-./nvidia-activate.fish
--- nvidia-x11/nvidia-remove.fish
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/fish
-
-# install
-yay -Rsc --noconfirm nvidia nvidia-utils nvidia-settings
-
-./nouveau-activate.fish
