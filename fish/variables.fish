73f9c54da67e5de2f513d95d96db9247add3c1f0
Rodrigo Dlugokenski <git@dlu.one>
2024-07-24 09:10:50 -0300

    vars and lazyvim updates

--- fish/variables.fish
+++ fish/variables.fish
@@ -6,7 +6,7 @@ test -e /usr/local/bin/micro; and set -Ux EDITOR micro
 set -gx BD_OPT insensitive
 
 mkdir -p $HOME/.local/bin $HOME/.fzf
-set -gx PATH $HOME/.local/bin $HOME/.yarn/bin $HOME/.cargo/bin $HOME/.local/npm/bin $PATH $HOME/.fzf
+set -gx PATH $HOME/.local/bin $HOME/.yarn/bin $HOME/.cargo/bin $HOME/.local/npm/bin /usr/lib64 $PATH $HOME/.fzf
 
 set -gx PROJECT_HOME $HOME/Projects
 
