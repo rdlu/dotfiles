67cb5aae04c22042d01f6b54da2b935f068b9359
Rodrigo Dlugokenski <git@dlu.one>
2024-07-26 08:40:36 -0300

    chatgpt keys, which key register -> add

--- lazyvim/.config/nvim/lua/plugins/gpt.lua
+++ lazyvim/.config/nvim/lua/plugins/gpt.lua
@@ -16,25 +16,26 @@ return {
     require("chatgpt").setup(cfg)
 
     local wk = require("which-key")
-    wk.register({
-      m = {
-        name = "ChatGPT",
-        c = { "<cmd>ChatGPT<CR>", "*ChatGPT*" },
-        a = { "<cmd>ChatGPTActAs<CR>", "ChatGPT Act As" },
-        e = { "<cmd>ChatGPTEditWithInstruction<CR>", "*Edit with instruction*" },
-        g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction" },
-        t = { "<cmd>ChatGPTRun translate<CR>", "Translate" },
-        k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords" },
-        d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring" },
-        T = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests" },
-        o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code" },
-        s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize" },
-        b = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs" },
-        x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code" },
-        r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit" },
-        y = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis" },
+    wk.add({
+      {
+        mode = { "n", "v" },
+        { "<leader>m", group = "ChatGPT" },
+        { "<leader>mT", "<cmd>ChatGPTRun add_tests<CR>", desc = "Add Tests" },
+        { "<leader>ma", "<cmd>ChatGPTActAs<CR>", desc = "ChatGPT Act As" },
+        { "<leader>mb", "<cmd>ChatGPTRun fix_bugs<CR>", desc = "Fix Bugs" },
+        { "<leader>mc", "<cmd>ChatGPT<CR>", desc = "*ChatGPT*" },
+        { "<leader>md", "<cmd>ChatGPTRun docstring<CR>", desc = "Docstring" },
+        { "<leader>me", "<cmd>ChatGPTEditWithInstruction<CR>", desc = "*Edit with instruction*" },
+        { "<leader>mg", "<cmd>ChatGPTRun grammar_correction<CR>", desc = "Grammar Correction" },
+        { "<leader>mk", "<cmd>ChatGPTRun keywords<CR>", desc = "Keywords" },
+        { "<leader>mo", "<cmd>ChatGPTRun optimize_code<CR>", desc = "Optimize Code" },
+        { "<leader>mr", "<cmd>ChatGPTRun roxygen_edit<CR>", desc = "Roxygen Edit" },
+        { "<leader>ms", "<cmd>ChatGPTRun summarize<CR>", desc = "Summarize" },
+        { "<leader>mt", "<cmd>ChatGPTRun translate<CR>", desc = "Translate" },
+        { "<leader>mx", "<cmd>ChatGPTRun explain_code<CR>", desc = "Explain Code" },
+        { "<leader>my", "<cmd>ChatGPTRun code_readability_analysis<CR>", desc = "Code Readability Analysis" },
       },
-    }, { prefix = "<leader>", mode = { "n", "v" } })
+    })
   end,
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
 }
