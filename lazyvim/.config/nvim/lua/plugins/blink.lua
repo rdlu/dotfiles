44fed1439c24f782f4c956c5c0959d3a8b3f9917
Rodrigo Dlugokenski <git@dlu.one>
2024-12-16 11:19:33 -0300

    disabling blink, too slow for elixir lexical

--- lazyvim/.config/nvim/lua/plugins/blink.lua
+++ lazyvim/.config/nvim/lua/plugins/blink.lua
@@ -1,9 +1,14 @@
 return {
-  "saghen/blink.cmp",
-  opts = {
-    keymap = { preset = "super-tab", ["<CR>"] = { "accept", "fallback" } },
-    sources = {
-      default = { "snippets", "lsp", "buffer", "path" },
-    },
-  },
+  -- "saghen/blink.cmp",
+  -- opts = {
+  --   keymap = { preset = "super-tab", ["<CR>"] = { "accept", "fallback" } },
+  --   sources = {
+  --     providers = {
+  --       lsp = {
+  --         async = true,
+  --       },
+  --     },
+  --     default = { "snippets", "lsp", "buffer", "path" },
+  --   },
+  -- },
 }
