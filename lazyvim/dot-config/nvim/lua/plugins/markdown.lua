c2db1f7c5f6b879c27afd0e0afffad95f9065c67
Rodrigo Dlugokenski <git@dlu.one>
2025-05-06 11:26:21 -0300

    lazyvim markdown render opts

---
+++ lazyvim/dot-config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,17 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    code = {
+      sign = true,
+      width = "full",
+      left_margin = 1,
+    },
+    heading = {
+      sign = true,
+      icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+    },
+    checkbox = {
+      enabled = true,
+    },
+  },
+}
