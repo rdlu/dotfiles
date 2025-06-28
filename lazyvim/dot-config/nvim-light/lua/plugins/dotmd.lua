66a94feeaa9f27e04ce2285f00f90345a8043b47
Rodrigo Dlugokenski <git@dlu.one>
2025-06-28 13:31:09 -0300

    dotmd wiki

---
+++ lazyvim/dot-config/nvim-light/lua/plugins/dotmd.lua
@@ -0,0 +1,10 @@
+-- dotmd.lua
+return {
+  "y3owk1n/dotmd.nvim",
+  version = "*", -- remove this if you want to use the `main` branch
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+}
