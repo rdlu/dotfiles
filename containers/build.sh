2da922f19f014dc2f8f8e85eecb26e971b0f5fce
Rodrigo Dlugokenski <git@dlu.one>
2025-07-06 14:59:46 -0300

    cleaning

--- containers/build.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-distrofolder="${1:-arch}"
-
-echo "!! Building $distrofolder toolbox image"
-podman build --tag $USER/$distrofolder ./$distrofolder
-echo "!! Creating $USER/$distrofolder toolbox container"
-
-toolbox create --image $USER/$distrofolder $distrofolder
